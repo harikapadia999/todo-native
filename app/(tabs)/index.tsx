f039f1cc2c38c0d54c79638ce240103efbaf95e6
harikapadia999 <harikapadia999@gmail.com>
2025-08-26 11:54:26 +0530

    feat: Add Convex API, server utilities, and schema for todos
    - Generated TypeScript definitions for API, data model, and server utilities.
    - Implemented schema definition for todos with fields: text and isCompleted.
    - Created query and mutation functions for managing todos (get, add, toggle, delete, update, clear).
    - Added TypeScript configuration for Convex functions.
    - Updated package.json and package-lock.json to include Convex dependency.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,14 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hook/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const { toggleDarkMode } = useTheme();
+  const todos = useQuery(api.todos.getTodos);
+  console.log(todos, "data");
+  const clearAll = useMutation(api.todos.clearAllTodos);
+  const addTodo = useMutation(api.todos.addTodo);
   return (
     <View
       style={{
@@ -13,7 +19,13 @@ export default function Index() {
     >
       <Text>Hello Reactnative!</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
-        <Text>Mode</Text>
+        <Text>Toggle Mode</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => addTodo({ text: "New Todo" })}>
+        <Text>Add todos</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => clearAll()}>
+        <Text>Clear all</Text>
       </TouchableOpacity>
     </View>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,18 @@
 import { ThemeProvider } from "@/hook/useTheme";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
 
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-    </ThemeProvider>
+    <ConvexProvider client={convex}>
+      <ThemeProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </ThemeProvider>
+    </ConvexProvider>
   );
 }
