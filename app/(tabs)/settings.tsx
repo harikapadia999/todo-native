26e8ff98f6b32dbef26c02351bf0599743488388
harikapadia999 <harikapadia999@gmail.com>
2025-08-28 13:56:54 +0530

    feat :: Add Settings screen components; implement Preferences and ProgressStats with UI enhancements and state management

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,17 +1,45 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { createSettingsStyles } from "@/assets/styles/settings.styles";
+import Preferences from "@/components/Preferences";
+import ProgressStats from "@/components/ProgressStats";
+import useTheme from "@/hook/useTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import React, { useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const SettingScreen = () => {
+  const [isAutoSync, setIsAutoSync] = useState(true);
+  const [isNotificationsEnabled, setisNotificationsEnabled] = useState(true);
+  const { colors, isDarkMode, toggleDarkMode } = useTheme();
+  const settingsStyles = createSettingsStyles(colors);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={settingsStyles.container}
     >
-      <Text>SettingScreen</Text>
-    </View>
+      <SafeAreaView style={settingsStyles.safeArea}>
+        <View style={settingsStyles.header}>
+          <View style={settingsStyles.titleContainer}>
+            <LinearGradient
+              colors={colors.gradients.primary}
+              style={settingsStyles.iconContainer}
+            >
+              <Ionicons name="settings" size={28} color="#fff" />
+            </LinearGradient>
+            <Text style={settingsStyles.title}>Settings</Text>
+          </View>
+        </View>
+        <ScrollView
+          style={settingsStyles.scrollView}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={settingsStyles.content}
+        >
+          <ProgressStats />
+          <Preferences />
+        </ScrollView>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
